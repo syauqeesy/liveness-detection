662daf84408c9e1cadcfc5bcb35384757654496f
Ahmad Syauqi <syauqeesy@gmail.com>
2026-09-23 21:25:48 +0700

    Add cors middleware

---
+++ middleware/cors.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/liveness-detection/configuration"
+)
+
+func Cors(configuration *configuration.Configuration) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Header.Get("Origin") == configuration.Application.Client {
+				w.Header().Set("Access-Control-Allow-Origin", configuration.Application.Client)
+				w.Header().Set("Vary", "Origin")
+			}
+
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
